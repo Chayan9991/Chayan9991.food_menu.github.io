15fd66ba255dc726024140e55db271ebaaa12ee7
Chayan Barman <barmanchayan10@gmail.com>
2024-12-14 12:36:19 +0530

    Update web build with web support enabled

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "727608678"
+    serviceWorkerVersion: "325240375"
   }
 });
